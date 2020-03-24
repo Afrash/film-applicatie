0680b822c9d6edfceb0c966c7dc9d7c6fef87021
2angelo3 <45686008+2angelo3@users.noreply.github.com>
2020-03-24 18:05:20 +0100

    Adding classes, attributes and getters

---
+++ app/src/main/java/com/example/filmapplicatie/SpokenLanguage.java
@@ -0,0 +1,16 @@
+package com.example.filmapplicatie;
+
+public class SpokenLanguage {
+    private String spokenLanguageName;
+
+
+
+
+    public String getSpokenLanguageName() {
+        return spokenLanguageName;
+    }
+
+    public void setSpokenLanguageName(String spokenLanguageName) {
+        this.spokenLanguageName = spokenLanguageName;
+    }
+}
