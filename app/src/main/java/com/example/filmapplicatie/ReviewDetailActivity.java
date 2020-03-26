403b490cf90996529833807615bde86949cb9665
2angelo3 <45686008+2angelo3@users.noreply.github.com>
2020-03-26 22:05:47 +0100

    Adding movie images & title to ReviewDetailActivity

--- app/src/main/java/com/example/filmapplicatie/ReviewDetailActivity.java
+++ app/src/main/java/com/example/filmapplicatie/ReviewDetailActivity.java
@@ -2,13 +2,19 @@ package com.example.filmapplicatie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ReviewDetailActivity extends AppCompatActivity {
 
     private static String TAG = ReviewDetailActivity.class.getName();
 
+    private TextView title;
+    private ImageView image;
     private TextView reviewId;
     private TextView ratingAverage;
     private TextView numberOfVotes;
@@ -19,6 +25,22 @@ public class ReviewDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_review_detail);
 
 
+        Intent intent = getIntent();
+
+
+
+
+        String mTitle = intent.getExtras().getString("TITLE");
+        String mImage = intent.getExtras().getString("IMAGE");
+
+
+        //set the image
+        Picasso.get()
+                .load("https://image.tmdb.org/t/p/w500" + mImage)
+                .into(image);
+
+
+        title.setText(mTitle);
 
 
     }
