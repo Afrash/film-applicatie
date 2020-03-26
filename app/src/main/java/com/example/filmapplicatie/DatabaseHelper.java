50ff5d5a169506cf0fb0fc580569367599cf2fd0
afrash <afrash.ramjit@hotmail.nl>
2020-03-26 13:59:10 +0100

    created attributes for listTable

--- app/src/main/java/com/example/filmapplicatie/DatabaseHelper.java
+++ app/src/main/java/com/example/filmapplicatie/DatabaseHelper.java
@@ -7,6 +7,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Lists";
+    public static final String TABLE_NAME = "rating_movies";
+    public static final String movieName = "movie_name";
+    public static final String ratingNumber = "rating_number";
+    public static final String review = "review";
+
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
